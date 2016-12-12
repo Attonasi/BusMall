250b351f771f46f461f948cdc117ef883013d30b
Attonasi <attonasi@hotmail.com>
2016-12-12 15:21:02 -0800

    I can make pictures appear

--- app1.js
+++ app1.js
@@ -3,11 +3,15 @@
 //start here
 
 // Globals
+var threePicturesID = document.getElementById('threePictures');
 var userChoices = 0;
+var lastLeftPic = null;
+var lastRightPic = null;
+var lastCenterPic = null;
 
-var imagePaths = ['/img/bag.jpg', '/img/banana.jpg', '/img/bathroom.jpg', '/img/boots.jpg', '/img/breakfast.jpg', '/img/bubblegum.jpg', '/img/chair.jpg', '/img/cthulu.jpg', '/img/dog-duck.jpg', '/img/dragon.jpg', '/img/pen.jpg', '/img/pet-sweep.jpg', '/img/scissors.jpg', '/img/shark.jpg', '/img/sweep.png', '/img/tauntaun.jpg', '/img/unicorn.jpg', '/img/usb.gif', '/img/water-can.jpg', '/img/wine-glass.jpg'];
+var imagePaths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 
-var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 var picArray = [];
 
@@ -27,8 +31,39 @@ function NewPicture(imagePath, imageName){
 
   picArray.push(this);
 }
+
 // Event Handler
 
+
+
+function superEventHandler(event){
+
+  event.preventDefault();
+
+
+  var leftPic = document.getElementById('leftPicture');
+  var centerPic = document.getElementById('centerPicture');
+  var rightPic = document.getElementById('rightPicture');
+
+  displayThreePictures(leftPic, centerPic, rightPic);
+
+}
 // Event Listener
-console.log(userChoices)
+threePicturesID.addEventListener('click', superEventHandler);
+
+console.log(userChoices);
 // Functions
+
+function displayThreePictures(leftPic, centerPic, rightPic){
+
+
+  leftPic.src = picArray[bigRandom()].filePath;
+  centerPic.src = picArray[bigRandom()].filePath;
+  rightPic.src = picArray[bigRandom()].filePath;
+
+
+}
+
+function bigRandom() {
+  return Math.floor(Math.random()*20);
+}
