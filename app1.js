9e910a82f35106ec5ee90da18590c7e9e3cef39e
Attonasi <attonasi@hotmail.com>
2016-12-12 17:47:40 -0800

    going home

--- app1.js
+++ app1.js
@@ -1,21 +1,18 @@
 'use strict';
 
-//start here
-
 // Globals
 var threePicturesID = document.getElementById('threePictures');
 var userChoices = 0;
-// var lastLeftPic = null;
-// var lastRightPic = null;
-// var lastCenterPic = null;
 var cantBe = [];
-
 var imagePaths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
-
 var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var leftPic = document.getElementById('leftPicture');
+var centerPic = document.getElementById('centerPicture');
+var rightPic = document.getElementById('rightPicture');
 
 var picArray = [];
 
+// fill picArray
 for(var i=0; i<imagePaths.length; i++){
   var imageName = imageNames[i];
   var imagePath = imagePaths[i];
@@ -24,7 +21,6 @@ for(var i=0; i<imagePaths.length; i++){
 
 // constructor
 function NewPicture(imagePath, imageName){
-
   this.name = imageName;
   this.filePath = imagePath;
   this.howOftenAppear = 0;
@@ -33,16 +29,46 @@ function NewPicture(imagePath, imageName){
   picArray.push(this);
 }
 
-// Event Handler
+
+displayThreePictures(leftPic, centerPic, rightPic);
+
+// Event Handler >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 function superEventHandler(event){
 
   event.preventDefault();
-  console.log('do something');
+  // console.log('do something');
+  if (event.currentTarget === event.target){
+    return alert('Click on the picture please');
+  }
 
-  var leftPic = document.getElementById('leftPicture');
-  var centerPic = document.getElementById('centerPicture');
-  var rightPic = document.getElementById('rightPicture');
+  console.log(event.target);
+  console.log(cantBe);
+  if (event.target === rightPic){
+    for(var z=0; z<picArray.length; z++){
+      console.log(picArray[z].filePath, cantBe[2]);
+      if (picArray[z].filePath===cantBe[2]){
+        picArray[z].howOftenClicked+=1;
+      }
+    }
+    console.log('rightPic clicked')
+  }
+  if (event.target === centerPic){
+    for(var x=0; x<picArray.length; x++){
+      if (picArray[x].filePath===cantBe[1]){
+        picArray[x].howOftenClicked+=1;
+      }
+    }
+    console.log('centerPic clicked')
+  }
+  if (event.target === leftPic){
+    for(var c=0; c<picArray.length; c++){
+      if (picArray[c].filePath===cantBe[0]){
+        picArray[c].howOftenClicked+=1;
+      }
+    }
+    console.log('leftPic clicked');
+  }
 
   displayThreePictures(leftPic, centerPic, rightPic);
 
@@ -54,7 +80,6 @@ console.log(userChoices);
 // Functions
 
 function displayThreePictures(leftPic, centerPic, rightPic){
-
 
   var leftPicSame = true;
   var centerPicSame = true;
@@ -99,7 +124,7 @@ function displayThreePictures(leftPic, centerPic, rightPic){
     cantBe.shift();
     cantBe.shift();
   }
-  console.log(cantBe);
+  // console.log(cantBe);
 }
 
 function bigRandom() {
