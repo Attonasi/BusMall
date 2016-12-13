0ba1b57c1e9221e5ecc079a9b39bfbc4e774a237
Attonasi <attonasi@hotmail.com>
2016-12-13 09:04:45 -0800

    this is what dreams are made of

--- app1.js
+++ app1.js
@@ -34,71 +34,58 @@ function NewPicture(imagePath, imageName){
 
 displayThreePictures(leftPic, centerPic, rightPic);
 
-
 // Functioning Functions
 
 function displayThreePictures(leftPic, centerPic, rightPic){
 
-  var leftPicSame = true;
-  var centerPicSame = true;
-  var rightPicSame  = true;
-  var leftPicIndex = -1;
-  var centerPicIndex = -1;
-  var rightPicIndex = -1;
-
-
-  while(leftPicSame){
-    leftPicIndex=bigRandom();
-    leftPic.src = picArray[leftPicIndex].filePath;
-    leftPic.alt = leftPicIndex;
-    for(var i=0; i<cantBe.length; i++){
-      if(cantBe[i]===leftPic.src){
-        leftPicIndex=bigRandom();
-        leftPic.src = picArray[leftPicIndex].filePath;
-        leftPic.alt = leftPicIndex;
-      }
+  // Left Picture Set
+  var leftPicIndex=bigRandom();
+  leftPic.src = picArray[leftPicIndex].filePath;
+  leftPic.alt = leftPicIndex;
+  for(var i=0; i<cantBe.length; i++){
+    if(cantBe[i]===leftPicIndex){
+      leftPicIndex=bigRandom();
+      leftPic.src = picArray[leftPicIndex].filePath;
+      leftPic.alt = leftPicIndex;
+      i=0;
     }
-    cantBe.push(leftPic.src);
-    picArray[leftPicIndex].howOftenAppear+=1;
-    leftPicSame = false;
   }
+  cantBe.push(leftPicIndex);
+  picArray[leftPicIndex].howOftenAppear+=1;
 
-  while(centerPicSame){
-    centerPicIndex=bigRandom();
-    centerPic.src = picArray[centerPicIndex].filePath;
-    centerPic.alt = centerPicIndex;
-    for(var j=0; j<cantBe.length; j++){
-      if(cantBe[j]===centerPic.src){
-        centerPicIndex=bigRandom();
-        centerPic.src = picArray[centerPicIndex].filePath;
-        centerPic.alt = centerPicIndex;
-      }
+  // Center Picture Set
+  var centerPicIndex=bigRandom();
+  centerPic.src = picArray[centerPicIndex].filePath;
+  centerPic.alt = centerPicIndex;
+  for(var j=0; j<cantBe.length; j++){
+    if(cantBe[j]===centerPicIndex){
+      centerPicIndex=bigRandom();
+      centerPic.src = picArray[centerPicIndex].filePath;
+      centerPic.alt = centerPicIndex;
+      j=0;
     }
-    cantBe.push(centerPic.src);
-    picArray[centerPicIndex].howOftenAppear+=1;
-    centerPicSame = false;
   }
+  cantBe.push(centerPicIndex);
+  picArray[centerPicIndex].howOftenAppear+=1;
 
-  while(rightPicSame){
-    rightPicIndex=bigRandom();
-    rightPic.src = picArray[rightPicIndex].filePath;
-    rightPic.alt = rightPicIndex;
-    for(var k=0; k<cantBe.length; k++){
-      if(cantBe[k]===rightPic.src){
-        rightPicIndex=bigRandom();
-        rightPic.src = picArray[rightPicIndex].filePath;
-        rightPic.alt = rightPicIndex;
-      }
+  // Right Picture Set
+  var rightPicIndex=bigRandom();
+  rightPic.src = picArray[rightPicIndex].filePath;
+  rightPic.alt = rightPicIndex;
+  for(var k=0; k<cantBe.length; k++){
+    if(cantBe[k]===rightPic.src){
+      rightPicIndex=bigRandom();
+      rightPic.src = picArray[rightPicIndex].filePath;
+      rightPic.alt = rightPicIndex;
+      k=0;
     }
-    cantBe.push(rightPic.src);
-    picArray[rightPicIndex].howOftenAppear+=1;
-    rightPicSame = false;
   }
+  cantBe.push(rightPicIndex);
+  picArray[rightPicIndex].howOftenAppear+=1;
+
   // console.log('The alts are '+leftPic.alt, centerPic.alt, rightPic.alt);
 
-  if(cantBe.length>3){
-    cantBe.shift();
-    cantBe.shift();
+  while(cantBe.length>3){
     cantBe.shift();
   }
 }
@@ -137,14 +124,14 @@ function superEventHandler(event){
 
 function buttonHandler(event){
   event.preventDefault();
-  if (resultsViewed){
+  if (resultsViewed === true){
     return alert('gosh you are annoying');
   }
   if(userChoices<25){
-    resultsViewed = true;
     return alert('Please complete the survey!')
   }
   console.table(picArray);
+  resultsViewed = true;
 }
 
 // Event Listeners
