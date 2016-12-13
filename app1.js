7bf170f91d40932f473a58016cc79bff7df3c2be
Attonasi <attonasi@hotmail.com>
2016-12-12 21:53:53 -0800

    got counters and display results button

--- app1.js
+++ app1.js
@@ -1,8 +1,10 @@
 'use strict';
 
-// Globals
+// Globals and data defined
+
 var threePicturesID = document.getElementById('threePictures');
-var userChoices = 0;
+var surveyForm = document.getElementById('survey-form');
+var userChoices = 1;
 var cantBe = [];
 var imagePaths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
@@ -32,8 +34,9 @@ function NewPicture(imagePath, imageName){
 
 displayThreePictures(leftPic, centerPic, rightPic);
 
-// Event Listener
+// Event Listeners
 threePicturesID.addEventListener('click', superEventHandler);
+surveyForm.addEventListener('submit', buttonHandler);
 
 // Event Handler >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -44,40 +47,12 @@ function superEventHandler(event){
   if (event.currentTarget === event.target){
     return alert('Click on the picture please');
   }
-  if(userChoices >=25)
+  if(userChoices >=5)
     return alert('Thank you for completing the survey! Please click on the view results button to review the results.')
 
   var clickedPic = event.target.id.value;
 
   console.log('event target is ' +clickedPic);
-
-
-  // console.log(cantBe);
-  // if (event.target === rightPic){
-  //   for(var z=0; z<picArray.length; z++){
-  //     console.log(picArray[z].filePath, cantBe[2]);
-  //     if (picArray[z].filePath===cantBe[2]){
-  //       picArray[z].howOftenClicked+=1;
-  //     }
-  //   }
-  //   console.log('rightPic clicked')
-  // }
-  // if (event.target === centerPic){
-  //   for(var x=0; x<picArray.length; x++){
-  //     if (picArray[x].filePath===cantBe[1]){
-  //       picArray[x].howOftenClicked+=1;
-  //     }
-  //   }
-  //   console.log('centerPic clicked')
-  // }
-  // if (event.target === leftPic){
-  //   for(var c=0; c<picArray.length; c++){
-  //     if (picArray[c].filePath===cantBe[0]){
-  //       picArray[c].howOftenClicked+=1;
-  //     }
-  //   }
-  //   console.log('leftPic clicked');
-  // }
 
   displayThreePictures(leftPic, centerPic, rightPic);
   userChoices+=1;
@@ -85,7 +60,17 @@ function superEventHandler(event){
 
 }
 
+function buttonHandler(event){
 
+  event.preventDefault();
+  if(userChoices<5){
+    return alert('Please complete the survey!')
+  }
+  // for(var v=0; v<picArray.length; v++){
+  //   document.write(picArray[v]);
+  // }
+  console.table(picArray);
+}
 
 // Functioning Functions
 
@@ -94,37 +79,50 @@ function displayThreePictures(leftPic, centerPic, rightPic){
   var leftPicSame = true;
   var centerPicSame = true;
   var rightPicSame  = true;
+  var leftPicIndex = -1;
+  var centerPicIndex = -1;
+  var rightPicIndex = -1;
+
 
   while(leftPicSame){
-    leftPic.src = picArray[bigRandom()].filePath;
+    leftPicIndex=bigRandom();
+    leftPic.src = picArray[leftPicIndex].filePath;
     for(var i=0; i<cantBe.length; i++){
       if(cantBe[i]===leftPic.src){
-        leftPic.src = picArray[bigRandom()].filePath;
+        leftPicIndex=bigRandom();
+        leftPic.src = picArray[leftPicIndex].filePath;
       }
     }
     cantBe.push(leftPic.src);
+    picArray[leftPicIndex].howOftenAppear+=1;
     leftPicSame = false;
   }
 
   while(centerPicSame){
-    centerPic.src = picArray[bigRandom()].filePath;
+    centerPicIndex=bigRandom();
+    centerPic.src = picArray[centerPicIndex].filePath;
     for(var j=0; j<cantBe.length; j++){
       if(cantBe[j]===centerPic.src){
-        centerPic.src = picArray[bigRandom()].filePath;
+        centerPicIndex=bigRandom();
+        centerPic.src = picArray[centerPicIndex].filePath;
       }
     }
     cantBe.push(centerPic.src);
+    picArray[centerPicIndex].howOftenAppear+=1;
     centerPicSame = false;
   }
 
   while(rightPicSame){
-    rightPic.src = picArray[bigRandom()].filePath;
+    rightPicIndex=bigRandom();
+    rightPic.src = picArray[rightPicIndex].filePath;
     for(var k=0; k<cantBe.length; k++){
       if(cantBe[k]===rightPic.src){
-        rightPic.src = picArray[bigRandom()].filePath;
+        rightPicIndex=bigRandom();
+        rightPic.src = picArray[rightPicIndex].filePath;
       }
     }
     cantBe.push(rightPic.src);
+    picArray[rightPicIndex].howOftenAppear+=1;
     rightPicSame = false;
   }
 
