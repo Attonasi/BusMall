bc433f1fea70925d143fde87302d88384b43fbdb
Attonasi <attonasi@hotmail.com>
2016-12-13 16:20:55 -0800

    charts are silly

--- app1.js
+++ app1.js
@@ -10,32 +10,17 @@ var currentArray = [];
 var resultsViewed = false;
 var imagePaths = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 var imageNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 var leftPic = document.getElementById('leftPicture');
 var centerPic = document.getElementById('centerPicture');
 var rightPic = document.getElementById('rightPicture');
-var barChart = document.getElementById('barChart');
+
+var barChart = document.getElementById('barChart').getContext('2d');
+
 var picArray = [];
+var appearChartData=[];
+var clickedChartData=[];
 
-var barChartData = {
-
-  labels : imageNames['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'chthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass']
-
-  datasets : [
-    {
-      fillColor : "red",
-      strokeColor : "green",
-      data : picArray.howOftenAppear;
-    },
-    {
-      fillColor : "blue",
-      strokeColor : "green)",
-      data : howOftenClicked;
-    }
-  ]
-}
-
-
-new Chart(barChart).Bar(barChartData)
 // fill picArray
 for(var i=0; i<imagePaths.length; i++){
   var imageName = imageNames[i];
@@ -98,6 +83,14 @@ function bigRandom() {
   return Math.floor(Math.random()*20);
 }
 
+function fillDataArrays(){
+  for(var w=0; w<picArray.length; w++){
+    appearChartData.push(picArray[w].howOftenAppear);
+    clickedChartData.push(picArray[w].howOftenClicked);
+  }
+  console.log(appearChartData, clickedChartData);
+}
+
 // Event Handlers >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 function superEventHandler(event){
@@ -117,7 +110,7 @@ function superEventHandler(event){
 
   userChoices+=1;
 
-  if(userChoices >=25){
+  if(userChoices >=5){
     return alert('Thank you for completing the survey! Please click on the view results button to review the results.');
   }
 
@@ -129,9 +122,74 @@ function buttonHandler(event){
   if (resultsViewed === true){
     return alert('gosh you are annoying');
   }
-  if(userChoices<25){
+  if(userChoices<5){
     return alert('Please complete the survey!')
   }
+  fillDataArrays();
+
+  // var myBarChart = new Chart(barChart, {
+    // type: 'bar',
+    // labels: imageNames,
+    // data: clickedChartData
+    // options: options
+
+
+  // });
+  var myChart = new Chart(barChart, {
+    type: 'bar',
+    data: {
+      labels: imageNames,
+      datasets: [{
+        label: 'Product Appeared',
+        data: appearChartData,
+        backgroundColor: 'red',
+              // ['rgba(255, 99, 132, 0.2)',
+              // 'rgba(54, 162, 235, 0.2)',
+              // 'rgba(255, 206, 86, 0.2)',
+              // 'rgba(75, 192, 192, 0.2)',
+              // 'rgba(153, 102, 255, 0.2)',
+              // 'rgba(255, 159, 64, 0.2)']
+
+        borderColor: 'black',
+              // ['rgba(255,99,132,1)',
+              // 'rgba(54, 162, 235, 1)',
+              // 'rgba(255, 206, 86, 1)',
+              // 'rgba(75, 192, 192, 1)',
+              // 'rgba(153, 102, 255, 1)',
+              // 'rgba(255, 159, 64, 1)']
+        borderWidth: 1
+      },
+        {
+          label: 'Product Chosen',
+          data: clickedChartData,
+          backgroundColor: 'blue',
+                // ['rgba(255, 99, 132, 0.2)',
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'],
+          borderColor: 'black',
+                // ['rgba(255,99,132,1)',
+                // 'rgba(54, 162, 235, 1)',
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'],
+          borderWidth: 1
+        }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+
   console.table(picArray);
   resultsViewed = true;
 }
